29cbd10e31d6a8729cf8f7399a00b452189fde0f
Md Shahazad Uddin <shanudjn@gmail.com>
2020-12-01 13:05:31 +0530

    added javascript to get the respective elements

---
+++ app.js
@@ -0,0 +1,7 @@
+var inputText = document.querySelector('#input-text');
+var btnTranslate = document.querySelector('#btn-translate')
+var outputArea = document.querySelector('#output-area');
+
+console.log(inputText);
+console.log(btnTranslate);
+console.log(outputArea);
