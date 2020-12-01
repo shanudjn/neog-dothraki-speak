629aaefb03b9c702bf28d2020071938415f66cf6
Md Shahazad Uddin <shanudjn@gmail.com>
2020-12-01 14:58:56 +0530

    added code fro click handling and fetching of data from API

--- app.js
+++ app.js
@@ -2,6 +2,36 @@ var inputText = document.querySelector('#input-text');
 var btnTranslate = document.querySelector('#btn-translate')
 var outputArea = document.querySelector('#output-area');
 
-console.log(inputText);
-console.log(btnTranslate);
-console.log(outputArea);
+
+var url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
+
+function getTranslateURL(text) {
+    return url + "?" + "text=" + text;
+}
+function displayTranslation(outputText) {
+    console.log(outputText)
+    outputArea.innerText = outputText //add the tranlsation to the reqd div
+}
+
+
+
+function errorHandler(error) {
+    console.log("Error occured" + error);
+}
+
+function clickHandler() {
+
+    let inputTextValue = inputText.value; //take value from input
+
+    fetch(getTranslateURL(inputTextValue))
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+
+            displayTranslation(json.contents.translated);
+        })
+        .catch(errorHandler);
+
+}
+//entry point
+btnTranslate.addEventListener("click", clickHandler);
